0f03f50f60c2f22cc79b774ec5de9448e44e2acf
Pedro Ferreira <pedro_hferreira1@hotmail.com>
2020-05-21 13:38:46 -0300

    Models and Interfaces

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1, "books");
+		Seller seller = new Seller(11, "nelson", "nelson@mail.com", new Date(), 3000.0, obj);
+
+		System.out.println(seller );
+	}
+
+}
